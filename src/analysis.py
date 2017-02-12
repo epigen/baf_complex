74d2d540cd60530a1be902f75f1b118a9df02e8b
Andre Rendeiro <afrendeiro@gmail.com>
2017-02-12 15:08:38 +0100

    add RNA-seq to analysis: collect expreession, normalize and unsupervised analysis

--- src/analysis.py
+++ src/analysis.py
@@ -515,6 +515,120 @@ class Analysis(object):
 
         return colors
 
+    def collect_bitseq_output(self, samples):
+        first = True
+        for i, sample in enumerate(samples):
+            if first:
+                try:
+                    # read the "tr" file of one sample to get indexes
+                    tr = pd.read_csv(
+                        os.path.join(
+                            sample.paths.sample_root, "bowtie1_{}".format(sample.transcriptome),
+                            "bitSeq",
+                            sample.name + ".tr"),
+                        sep=" ", header=None, skiprows=1,
+                        names=["ensembl_gene_id", "ensembl_transcript_id", "v1", "v2"])
+                except IOError:
+                    print("Sample {} is missing.".format(sample.name))
+                    continue
+                # add id index
+                tr.set_index("ensembl_gene_id", append=False, inplace=True)
+                tr.set_index("ensembl_transcript_id", append=True, inplace=True)
+                # create dataframe
+                expr = pd.DataFrame(index=tr.index)
+                first = False
+
+            # load counts
+            try:
+                counts = pd.read_csv(os.path.join(
+                    sample.paths.sample_root, "bowtie1_{}".format(sample.transcriptome),
+                    "bitSeq",
+                    sample.name + ".counts"), sep=" ")
+            except IOError:
+                print("Sample {} is missing.".format(sample.name))
+                continue
+            counts.index = expr.index
+
+            # Append
+            expr[sample.name] = counts
+
+        return expr
+
+    def get_gene_expression(self, samples=None, attributes=["sample_name", "cell_line", "knockout", "replicate", "clone", "complex_subunit"]):
+        """
+        """
+        def quantile_normalize(df_input):
+            df = df_input.copy()
+            # compute rank
+            dic = {}
+            for col in df:
+                dic.update({col: sorted(df[col])})
+            sorted_df = pd.DataFrame(dic)
+            rank = sorted_df.mean(axis=1).tolist()
+            # sort
+            for col in df:
+                t = np.searchsorted(np.sort(df[col]), df[col])
+                df[col] = [rank[i] for i in t]
+            return df
+
+        if samples is None:
+            samples = [s for s in self.samples if s.library == "RNA-seq"]
+
+        self.count_matrix = self.collect_bitseq_output(samples)
+
+        # Map ensembl gene IDs to gene names
+        import requests
+        url_query = "".join([
+            """http://ensembl.org/biomart/martservice?query=""",
+            """<?xml version="1.0" encoding="UTF-8"?>""",
+            """<!DOCTYPE Query>""",
+            """<Query  virtualSchemaName = "default" formatter = "CSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >""",
+            """<Dataset name = "hsapiens_gene_ensembl" interface = "default" >""",
+            """<Attribute name = "ensembl_gene_id" />""",
+            """<Attribute name = "external_gene_name" />""",
+            """</Dataset>""",
+            """</Query>"""])
+        req = requests.get(url_query, stream=True)
+        mapping = pd.DataFrame((x.strip().split(",") for x in list(req.iter_lines())), columns=["ensembl_gene_id", "gene_name"]).set_index("ensembl_gene_id").to_dict()['gene_name']
+        index = self.count_matrix.index.get_level_values('ensembl_gene_id').str.replace("\..*", "")
+        genes = [mapping[g] for g in index[index.isin(mapping.keys())]]
+
+        self.count_matrix = self.count_matrix[index.isin(mapping.keys())]
+        self.count_matrix.index = pd.MultiIndex.from_arrays([genes, self.count_matrix.index.get_level_values('ensembl_gene_id')], names=['gene_name', "ensembl_transcript_id"])
+
+        # Reduce to gene-level measurements by max of transcripts
+        self.expression_matrix_counts = self.count_matrix.groupby(level=["gene_name"]).max()
+
+        # Quantile normalize
+        self.expression_matrix_qnorm = quantile_normalize(self.expression_matrix_counts)
+
+        # Log2 TPM
+        self.expression = np.log2((1 + self.expression_matrix_qnorm) / self.expression_matrix_qnorm.sum(axis=0) * 1e6)
+
+        # Annotate with sample metadata
+        _samples = [s for s in samples if s.name in self.expression.columns]
+        attrs = list()
+        for attr in attributes:
+            l = list()
+            for sample in _samples:  # keep order of samples in matrix
+                try:
+                    l.append(getattr(sample, attr))
+                except AttributeError:
+                    l.append(np.nan)
+            attrs.append(l)
+
+        # Generate multiindex columns
+        index = pd.MultiIndex.from_arrays(attrs, names=attributes)
+        self.expression_annotated = self.expression[[s.name for s in _samples]]
+        self.expression_annotated.columns = index
+
+        # Save
+        self.count_matrix.to_csv(os.path.join(self.results_dir, self.name + ".expression_counts.csv"), index=True)
+        self.expression_matrix_counts.to_csv(os.path.join(self.results_dir, self.name + ".expression_counts.gene_level.csv"), index=True)
+        self.expression_matrix_qnorm.to_csv(os.path.join(self.results_dir, self.name + ".expression_counts.gene_level.quantile_normalized.csv"), index=True)
+        self.expression.to_csv(os.path.join(self.results_dir, self.name + ".expression_counts.gene_level.quantile_normalized.log2_tpm.csv"), index=True)
+        self.expression_annotated.to_csv(os.path.join(self.results_dir, self.name + ".expression_counts.gene_level.quantile_normalized.log2_tpm.annotated_metadata.csv"), index=True)
+
     def plot_peak_characteristics(self):
         # Loop at summary statistics:
         # interval lengths
@@ -742,6 +856,42 @@ class Analysis(object):
         sns.jointplot('mean', "qv2", data=filtered)
         plt.savefig(os.path.join(self.results_dir, "norm_counts_per_sample.support_vs_qv2.filtered.png"), bbox_inches="tight", dpi=300)
 
+    def plot_expression_characteristics(self):
+        fig, axis = plt.subplots(figsize=(4, 4 * np.log10(len(self.expression_matrix_counts.columns))))
+        sns.barplot(data=self.expression_matrix_counts.sum().sort_values().reset_index(), y="index", x=0, orient="horiz", color=sns.color_palette("colorblind")[0], ax=axis)
+        axis.set_xlabel("Reads")
+        axis.set_ylabel("Samples")
+        sns.despine(fig)
+        fig.savefig(os.path.join(self.results_dir, "expression.reads_per_sample.svg"), bbox_inches="tight")
+
+        cov = pd.DataFrame()
+        for i in [1, 2, 3, 6, 12, 24, 48, 96, 200, 300, 400, 500, 1000]:
+            cov[i] = self.expression_matrix_counts.apply(lambda x: sum(x >= i))
+
+        fig, axis = plt.subplots(1, 2, figsize=(6 * 2, 6))
+        sns.heatmap(cov.drop([1, 2], axis=1), ax=axis[0], cmap="GnBu", cbar_kws={"label": "Genes covered"})
+        sns.heatmap(cov.drop([1, 2], axis=1).apply(lambda x: (x - x.mean()) / x.std(), axis=0), ax=axis[1], cbar_kws={"label": "Z-score"})
+        for ax in axis:
+            ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+            ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+            ax.set_ylabel("Samples")
+            ax.set_xlabel("Genes with #reads")
+        axis[1].set_yticklabels(ax.get_yticklabels(), visible=False)
+        sns.despine(fig)
+        fig.savefig(os.path.join(self.results_dir, "expression.genes_with_reads.svg"), bbox_inches="tight")
+
+        for name, matrix in [("counts", self.expression_matrix_counts), ("qnorm_TPM", self.expression)]:
+            # Boxplot with values per sample
+            to_plot = pd.melt(matrix, var_name="sample")
+
+            fig, axis = plt.subplots()
+            sns.boxplot(data=to_plot, y="sample", x="value", orient="horiz", ax=axis)
+            axis.set_xlim(0, 2000)  # cut it at 2kb
+            axis.set_xlabel("peak width (bp)")
+            axis.set_ylabel("frequency")
+            sns.despine(fig)
+            fig.savefig(os.path.join(self.results_dir, "expression.boxplot_per_sample.{}.svg".format(name)), bbox_inches="tight")
+
     def unsupervised(
             self, samples, attributes=["cell_line", "knockout", "replicate", "clone"],
             exclude=[]):
@@ -758,7 +908,7 @@ class Analysis(object):
         color_dataframe = pd.DataFrame(self.get_level_colors(levels=attributes), index=attributes, columns=[s.name for s in self.samples])
 
         # exclude samples if needed
-        samples = [s for s in samples if s.name not in exclude]
+        samples = [s for s in samples if s.name not in exclude and s.library == "ATAC-seq"]
         color_dataframe = color_dataframe[[s.name for s in samples]]
         sample_display_names = color_dataframe.columns.str.replace("_ATAC-seq", "").str.replace("_hg19", "")
 
@@ -1136,6 +1286,194 @@ class Analysis(object):
             for tick in g.ax_heatmap.get_yticklabels():
                 tick.set_rotation(0)
             g.fig.savefig(os.path.join("results", "{}.PCA.PC_pvalues.enrichr_enrichments.{}.z_score.svg".format(self.name, gene_set_library)), bbox_inches="tight", dpi=300)
+
+    def unsupervised_expression(
+            self, samples, attributes=["cell_line", "knockout", "replicate", "clone"],
+            exclude=["RNA-seq_HAP1_WT_r1_GFP", "RNA-seq_HAP1_WT_r2_GFP", "RNA-seq_HAP1_WT_r3_GFP", "RNA-seq_HAP1_WT_r4_GFP"]):
+        """
+        """
+        from sklearn.decomposition import PCA
+        from sklearn.manifold import MDS
+        from collections import OrderedDict
+        import re
+        import itertools
+        from scipy.stats import kruskal
+        from scipy.stats import pearsonr
+
+        samples = [s for s in samples if s.name in self.expression_annotated.columns.get_level_values("sample_name") and s.library == "RNA-seq"]
+
+        color_dataframe = pd.DataFrame(
+            self.get_level_colors(index=self.expression_annotated.columns, levels=attributes), index=attributes,
+            columns=[s.name for s in samples])
+
+        # exclude samples if needed
+        samples = [s for s in samples if s.name not in exclude]
+        color_dataframe = color_dataframe[[s.name for s in samples]]
+        sample_display_names = color_dataframe.columns.str.replace("_ATAC-seq", "").str.replace("_hg19", "")
+
+        # exclude attributes if needed
+        to_plot = attributes[:]
+        to_exclude = ["patient_age_at_collection", "sample_name"]
+        for attr in to_exclude:
+            try:
+                to_plot.pop(to_plot.index(attr))
+            except:
+                continue
+
+        color_dataframe = color_dataframe.ix[to_plot]
+
+        # All regions
+        X = self.expression_annotated[[s.name for s in samples if s.name not in exclude]]
+
+        # Pairwise correlations
+        g = sns.clustermap(
+            X.corr() ** 2, xticklabels=False, yticklabels=sample_display_names, annot=True,
+            cmap="Spectral_r", figsize=(15, 15), cbar_kws={"label": "Pearson correlation"}, row_colors=color_dataframe.values.tolist(), vmin=0.9)
+        for item in g.ax_heatmap.get_yticklabels():
+            item.set_rotation(0)
+        g.ax_heatmap.set_xlabel(None)
+        g.ax_heatmap.set_ylabel(None)
+        g.fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.corr.clustermap.svg".format(self.name)), bbox_inches='tight')
+
+        # MDS
+        mds = MDS(n_jobs=-1)
+        x_new = mds.fit_transform(X.T)
+        # transform again
+        x = pd.DataFrame(x_new)
+        xx = x.apply(lambda j: (j - j.mean()) / j.std(), axis=0)
+
+        fig, axis = plt.subplots(1, len(to_plot), figsize=(4 * len(to_plot), 4 * 1))
+        axis = axis.flatten()
+        for i, attr in enumerate(to_plot):
+            for j in range(len(xx)):
+                try:
+                    label = getattr(samples[j], to_plot[i])
+                except AttributeError:
+                    label = np.nan
+                axis[i].scatter(xx.ix[j][0], xx.ix[j][1], s=50, color=color_dataframe.ix[attr][j], label=label)
+            axis[i].set_title(to_plot[i])
+            axis[i].set_xlabel("MDS 1")
+            axis[i].set_ylabel("MDS 2")
+            axis[i].set_xticklabels(axis[i].get_xticklabels(), visible=False)
+            axis[i].set_yticklabels(axis[i].get_yticklabels(), visible=False)
+
+            # Unique legend labels
+            handles, labels = axis[i].get_legend_handles_labels()
+            by_label = OrderedDict(zip(labels, handles))
+            if any([type(c) in [str, unicode] for c in by_label.keys()]) and len(by_label) <= 30:
+                if not any([re.match("^\d", c) for c in by_label.keys()]):
+                    axis[i].legend(by_label.values(), by_label.keys())
+        fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.mds.svg".format(self.name)), bbox_inches="tight")
+
+        # PCA
+        pca = PCA()
+        x_new = pca.fit_transform(X.T)
+        # transform again
+        x = pd.DataFrame(x_new)
+        xx = x.apply(lambda j: (j - j.mean()) / j.std(), axis=0)
+
+        # plot % explained variance per PC
+        fig, axis = plt.subplots(1)
+        axis.plot(
+            range(1, len(pca.explained_variance_) + 1),  # all PCs
+            (pca.explained_variance_ / pca.explained_variance_.sum()) * 100, 'o-')  # % of total variance
+        axis.axvline(len(to_plot), linestyle='--')
+        axis.set_xlabel("PC")
+        axis.set_ylabel("% variance")
+        sns.despine(fig)
+        fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.pca.explained_variance.svg".format(self.name)), bbox_inches='tight')
+
+        # plot
+        pcs = min(xx.shape[0] - 1, 10)
+        fig, axis = plt.subplots(pcs, len(to_plot), figsize=(4 * len(to_plot), 4 * pcs))
+        for pc in range(pcs):
+            for i, attr in enumerate(to_plot):
+                for j in range(len(xx)):
+                    try:
+                        label = getattr(samples[j], to_plot[i])
+                    except AttributeError:
+                        label = np.nan
+                    axis[pc, i].scatter(xx.ix[j][pc], xx.ix[j][pc + 1], s=50, color=color_dataframe.ix[attr][j], label=label)
+                axis[pc, i].set_title(to_plot[i])
+                axis[pc, i].set_xlabel("PC {}".format(pc + 1))
+                axis[pc, i].set_ylabel("PC {}".format(pc + 2))
+                axis[pc, i].set_xticklabels(axis[pc, i].get_xticklabels(), visible=False)
+                axis[pc, i].set_yticklabels(axis[pc, i].get_yticklabels(), visible=False)
+
+                # Unique legend labels
+                handles, labels = axis[pc, i].get_legend_handles_labels()
+                by_label = OrderedDict(zip(labels, handles))
+                if any([type(c) in [str, unicode] for c in by_label.keys()]) and len(by_label) <= 30:
+                    if not any([re.match("^\d", c) for c in by_label.keys()]):
+                        axis[pc, i].legend(by_label.values(), by_label.keys())
+        fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.pca.svg".format(self.name)), bbox_inches="tight")
+
+        #
+
+        # # Test association of PCs with attributes
+        associations = list()
+        for pc in range(pcs):
+            for attr in attributes[1:]:
+                print("PC {}; Attribute {}.".format(pc + 1, attr))
+                sel_samples = [s for s in samples if hasattr(s, attr)]
+                sel_samples = [s for s in sel_samples if not pd.isnull(getattr(s, attr))]
+
+                # Get all values of samples for this attr
+                groups = set([getattr(s, attr) for s in sel_samples])
+
+                # Determine if attr is categorical or continuous
+                if all([type(i) in [str, bool] for i in groups]) or len(groups) == 2:
+                    variable_type = "categorical"
+                elif all([type(i) in [int, float, np.int64, np.float64] for i in groups]):
+                    variable_type = "numerical"
+                else:
+                    print("attr %s cannot be tested." % attr)
+                    associations.append([pc + 1, attr, variable_type, np.nan, np.nan, np.nan])
+                    continue
+
+                if variable_type == "categorical":
+                    # It categorical, test pairwise combinations of attributes
+                    for group1, group2 in itertools.combinations(groups, 2):
+                        g1_indexes = [i for i, s in enumerate(sel_samples) if getattr(s, attr) == group1]
+                        g2_indexes = [i for i, s in enumerate(sel_samples) if getattr(s, attr) == group2]
+
+                        g1_values = xx.loc[g1_indexes, pc]
+                        g2_values = xx.loc[g2_indexes, pc]
+
+                        # Test ANOVA (or Kruskal-Wallis H-test)
+                        p = kruskal(g1_values, g2_values)[1]
+
+                        # Append
+                        associations.append([pc + 1, attr, variable_type, group1, group2, p])
+
+                elif variable_type == "numerical":
+                    # It numerical, calculate pearson correlation
+                    indexes = [i for i, s in enumerate(samples) if s in sel_samples]
+                    pc_values = xx.loc[indexes, pc]
+                    trait_values = [getattr(s, attr) for s in sel_samples]
+                    p = pearsonr(pc_values, trait_values)[1]
+
+                    associations.append([pc + 1, attr, variable_type, np.nan, np.nan, p])
+
+        associations = pd.DataFrame(associations, columns=["pc", "attribute", "variable_type", "group_1", "group_2", "p_value"])
+
+        # write
+        associations.to_csv(os.path.join(self.results_dir, "{}.expression.all_sites.pca.variable_principle_components_association.csv".format(self.name)), index=False)
+
+        # Plot
+        # associations[associations['p_value'] < 0.05].drop(['group_1', 'group_2'], axis=1).drop_duplicates()
+        # associations.drop(['group_1', 'group_2'], axis=1).drop_duplicates().pivot(index="pc", columns="attribute", values="p_value")
+        pivot = associations.groupby(["pc", "attribute"]).min()['p_value'].reset_index().pivot(index="pc", columns="attribute", values="p_value").dropna(axis=1)
+
+        # heatmap of -log p-values
+        g = sns.clustermap(-np.log10(pivot), row_cluster=False, annot=True, cbar_kws={"label": "-log10(p_value) of association"})
+        g.ax_heatmap.set_xticklabels(g.ax_heatmap.get_xticklabels(), rotation=45, ha="right")
+        g.fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.pca.variable_principle_components_association.svg".format(self.name)), bbox_inches="tight")
+
+        # heatmap of masked significant
+        g = sns.clustermap((pivot < 0.05).astype(int), row_cluster=False, cbar_kws={"label": "significant association"})
+        g.ax_heatmap.set_xticklabels(g.ax_heatmap.get_xticklabels(), rotation=45, ha="right")
+        g.fig.savefig(os.path.join(self.results_dir, "{}.expression.all_sites.pca.variable_principle_components_association.masked.svg".format(self.name)), bbox_inches="tight")
 
     def differential_region_analysis(
             self, samples, trait="knockout",
