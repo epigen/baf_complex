ebb17148850051bf2088e9ef46ac86b8d288d4ae
Andre Rendeiro <afrendeiro@gmail.com>
2017-03-04 20:50:48 +0100

    compute phasograms from pairwise dyad distances and call peaks on the distances

--- src/analysis.py
+++ src/analysis.py
@@ -3382,6 +3382,63 @@ def investigate_nucleosome_positions(self, samples, cluster=True):
     g.savefig(os.path.join(self.results_dir, "nucleoatac", "open_chromatin_coverage.norm_mean_coverage.smooth.svg"), bbox_inches="tight")
 
 
+def phasograms(self, samples, max_dist=10000, rolling_window=50, plotting_window=(0, 500)):
+    df = self.prj.sheet.df[self.prj.sheet.df["library"] == "ATAC-seq"]
+    groups = list()
+    for attrs, index in df.groupby(["library", "cell_line", "knockout", "clone"]).groups.items():
+        name = "_".join([a for a in attrs if not pd.isnull(a)])
+        groups.append(name)
+    groups = sorted(groups)
+
+    def difference_matrix(a):
+        x = np.reshape(a, (len(a), 1))
+        return x - x.transpose()
+
+    distances = dict()
+
+    for group in groups:
+        print(group)
+        # Get dyad calls from nucleoatac
+        df = pd.read_csv(os.path.join(self.results_dir, "nucleoatac", group, group + ".nucpos.bed.gz"), sep="\t", header=None)
+
+        # separate by chromosome (groupby?)
+        # count pairwise distance
+        dists = list()
+        for chrom in df[0].unique():
+            d = abs(difference_matrix(df[df[0] == chrom][1]))
+            dd = d[(d < max_dist) & (d != 0)]
+            dists += dd.tolist()
+        distances[group] = dists
+
+    pickle.dump(distances, open(os.path.join(self.results_dir, "nucleoatac", "phasogram.distances.pickle"), "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+
+    n_rows = n_cols = int(np.ceil(np.sqrt(len(groups))))
+    n_rows -= 1
+    fig, axis = plt.subplots(n_rows, n_cols, sharex=True, sharey=True, figsize=(n_cols * 3, n_rows * 2))
+    axis = axis.flatten()
+    for i, group in enumerate(groups):
+        # Count frequency of dyad distances
+        x = pd.Series(distances[group])
+        y = x.value_counts().sort_index()
+        y = y.ix[plotting_window[0], plotting_window[1]]
+        y /= y.sum()
+
+        # Find peaks
+        y2 = y.rolling(rolling_window).mean()
+        y2.index = y2.index - rolling_window
+        peak_indices = detect_peaks(y2.values, mpd=73.5)[:3]
+        print(group, y2.iloc[peak_indices].index)
+
+        # Plot distribution and peaks
+        axis[i].plot(y2.index, y2)
+        axis[i].scatter(y2.iloc[peak_indices].index, y2.iloc[peak_indices], s=25, color="orange")
+        for peak in y2.iloc[peak_indices].index:
+            axis[i].axvline(peak, color="black", linestyle="--")
+        axis[i].set_title(group)
+    sns.despine(fig)
+    fig.savefig(os.path.join(self.results_dir, "nucleoatac", "phasograms.50bp_rolling_mean.peaks.svg"), bbox_inches="tight")
+
+
 def run_coverage_job(bed_file, bam_file, coverage_type, name, output_dir):
     from pypiper import NGSTk
     tk = NGSTk()
@@ -3533,6 +3590,136 @@ date
     tk.slurm_submit_job(job_file)
 
 
+def detect_peaks(x, mph=None, mpd=1, threshold=0, edge='rising',
+                 kpsh=False, valley=False):
+
+    """Detect peaks in data based on their amplitude and other features.
+
+    Parameters
+    ----------
+    x : 1D array_like
+        data.
+    mph : {None, number}, optional (default = None)
+        detect peaks that are greater than minimum peak height.
+    mpd : positive integer, optional (default = 1)
+        detect peaks that are at least separated by minimum peak distance (in
+        number of data).
+    threshold : positive number, optional (default = 0)
+        detect peaks (valleys) that are greater (smaller) than `threshold`
+        in relation to their immediate neighbors.
+    edge : {None, 'rising', 'falling', 'both'}, optional (default = 'rising')
+        for a flat peak, keep only the rising edge ('rising'), only the
+        falling edge ('falling'), both edges ('both'), or don't detect a
+        flat peak (None).
+    kpsh : bool, optional (default = False)
+        keep peaks with same height even if they are closer than `mpd`.
+    valley : bool, optional (default = False)
+        if True (1), detect valleys (local minima) instead of peaks.
+    show : bool, optional (default = False)
+        if True (1), plot data in matplotlib figure.
+    ax : a matplotlib.axes.Axes instance, optional (default = None).
+
+    Returns
+    -------
+    ind : 1D array_like
+        indeces of the peaks in `x`.
+
+    Notes
+    -----
+    The detection of valleys instead of peaks is performed internally by simply
+    negating the data: `ind_valleys = detect_peaks(-x)`
+
+    The function can handle NaN's
+
+    See this IPython Notebook [1]_.
+
+    References
+    ----------
+    .. [1] http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
+
+    Examples
+    --------
+    >>> from detect_peaks import detect_peaks
+    >>> x = np.random.randn(100)
+    >>> x[60:81] = np.nan
+    >>> # detect all peaks and plot data
+    >>> ind = detect_peaks(x, show=True)
+    >>> print(ind)
+
+    >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
+    >>> # set minimum peak height = 0 and minimum peak distance = 20
+    >>> detect_peaks(x, mph=0, mpd=20, show=True)
+
+    >>> x = [0, 1, 0, 2, 0, 3, 0, 2, 0, 1, 0]
+    >>> # set minimum peak distance = 2
+    >>> detect_peaks(x, mpd=2, show=True)
+
+    >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
+    >>> # detection of valleys instead of peaks
+    >>> detect_peaks(x, mph=0, mpd=20, valley=True, show=True)
+
+    >>> x = [0, 1, 1, 0, 1, 1, 0]
+    >>> # detect both edges
+    >>> detect_peaks(x, edge='both', show=True)
+
+    >>> x = [-2, 1, -2, 2, 1, 1, 3, 0]
+    >>> # set threshold = 2
+    >>> detect_peaks(x, threshold = 2, show=True)
+    """
+
+    x = np.atleast_1d(x).astype('float64')
+    if x.size < 3:
+        return np.array([], dtype=int)
+    if valley:
+        x = -x
+    # find indices of all peaks
+    dx = x[1:] - x[:-1]
+    # handle NaN's
+    indnan = np.where(np.isnan(x))[0]
+    if indnan.size:
+        x[indnan] = np.inf
+        dx[np.where(np.isnan(dx))[0]] = np.inf
+    ine, ire, ife = np.array([[], [], []], dtype=int)
+    if not edge:
+        ine = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) > 0))[0]
+    else:
+        if edge.lower() in ['rising', 'both']:
+            ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
+        if edge.lower() in ['falling', 'both']:
+            ife = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) >= 0))[0]
+    ind = np.unique(np.hstack((ine, ire, ife)))
+    # handle NaN's
+    if ind.size and indnan.size:
+        # NaN's and values close to NaN's cannot be peaks
+        ind = ind[np.in1d(ind, np.unique(np.hstack((indnan, indnan - 1, indnan + 1))), invert=True)]
+    # first and last values of x cannot be peaks
+    if ind.size and ind[0] == 0:
+        ind = ind[1:]
+    if ind.size and ind[-1] == x.size - 1:
+        ind = ind[:-1]
+    # remove peaks < minimum peak height
+    if ind.size and mph is not None:
+        ind = ind[x[ind] >= mph]
+    # remove peaks - neighbors < threshold
+    if ind.size and threshold > 0:
+        dx = np.min(np.vstack([x[ind] - x[ind - 1], x[ind] - x[ind + 1]]), axis=0)
+        ind = np.delete(ind, np.where(dx < threshold)[0])
+    # detect small peaks closer than minimum peak distance
+    if ind.size and mpd > 1:
+        ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
+        idel = np.zeros(ind.size, dtype=bool)
+        for i in range(ind.size):
+            if not idel[i]:
+                # keep peaks with the same height if kpsh is True
+                idel = idel | (ind >= ind[i] - mpd) & (ind <= ind[i] + mpd) \
+                    & (x[ind[i]] > x[ind] if kpsh else True)
+                idel[i] = 0  # Keep current peak
+        # remove the small peaks and sort back the indices by their occurrence
+        ind = np.sort(ind[~idel])
+
+    return ind
+
+
 def add_args(parser):
     """
     Options for project and pipelines.
@@ -3612,7 +3799,11 @@ def main():
     #
 
     # Supervised analysis
-    analysis.differential_region_analysis(atacseq_samples)
+    analysis.differential_region_analysis(
+        atacseq_samples,
+        trait="knockout",
+        variables=["knockout", "replicate"],
+        output_suffix="deseq_knockout")
 
     # Investigate global changes in accessibility
     global_changes(atacseq_samples)
